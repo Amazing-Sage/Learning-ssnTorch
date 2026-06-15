70a2e4f5272504d598de913f4a4ed7b142fd80f6
Your Name <you@example.com>
2026-06-15 10:39:26 -0700

    loading MNIST,define transforms,create dataloaders

--- main.py
+++ main.py
@@ -25,5 +25,37 @@ device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"
         #   checks if the Navidas graphics card with CUDA drivers are installed
 
 #load MNIST darasheet 
+# this is part of loading essential tools needed to load and prepocess a dataset
+
 from torch.utils.data import DataLoader
-from torchvision import datasheets, transforms
+    #this takes large amount of data and splits it into small "bite sized data" to 
+    #prevent crashing. it also shuggles data so network doesn't memorize order of the 
+    #images it's trying to guess. 
+
+from torchvision import datasets, transforms
+    #torchvision.transforms  converts image into PyTorch numbers
+
+    # "datasets" loads a large library of popular datasets to use as images and orginizes them
+    #into folders.
+    
+    #"transforms" uses raw images from the internet in JPEG or PNG. 
+    #However, it can't read JPEG so it will modigy the images converting 
+    #them into math friendly numbers and adjusting the image so AI can read it 
+
+#define a transform
+    #this creates a list of modifications to every image for the AI to read
+transform = transforms.Compose([
+        transform.Resize((28,28)). #28 x 28 pixles
+        transform.Greyscale(), #makes only black and white image
+        transform.ToTensor(), #converts image to math
+        transforms.Normalize((0,),(1,)) #squishes pixle values between math rage to help learn faster
+    ])
+        
+
+#mnist dowloads data to computer
+mnist_train= datasets.MNIST(data_path, train=True,download=True,transform=transform)
+mnist_test=datasets.MNIST(data_path,train=False,download=True,transform=transform)
+
+#create Dataloaders (feeds network images)
+train_loader= DataLoader(mnist_train,batch_size=batch_size,shuffle=True)
+test_loader= DataLoader(mnist_test, batch_size=batch_size,shuffle=True)
