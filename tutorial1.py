83692345eb1874a12e1a9c60ece9f7fa39cf1c03
Amazing-Sage <baker.26.virginia@gmail.com>
2026-06-17 10:26:22 -0700

    downloading datasets

--- tutorial1.py
+++ tutorial1.py
@@ -19,5 +19,29 @@ num_classes=10 #MNIST has 10 output classes
 #torch Variables 
 dtype = torch.float
  
+#________________________________________________________________________
+#Download Dataset
+#________________________________________________________________________
+from torchvision import datasets, transforms
+
+#define a transform to normalize the data 
+transform = transforms.Compose([
+    transforms.Resize((28, 28)),
+    transforms.Greyscale(),
+    transforms.ToTensor(),
+    transforms.Normalize((0,), (1,))
+])
+
+mnist_train=datasets.MNIST(data_path,train=True, download=True, transform=transform)
+
+#start traning networks, data_subset reduces the dataset by the factor defined in subset
+
+from snntorch import utils 
+
+subset=10
+mnist_train=utils.data_subset(mnist_train, subset)
+print(f"The size of mnist is {len(mnist_train)}")
+#number should be 60k, this confirms the number of hadwritten digit images avalible to train machine
+
 
 
