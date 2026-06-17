da05328dbfca05011368267893d458be14a53290
Amazing-Sage <baker.26.virginia@gmail.com>
2026-06-17 16:07:49 -0700

    starting Lapicqes

--- tutorial2.py
+++ tutorial2.py
@@ -13,18 +13,33 @@
 import snntorch as snn 
 from snntorch import spikeplot as splt
 from snntorch import spikegen
-
 import torch 
 import torch.nn as nn 
-
 import numpy as np 
-
 import matplotlib.pyplot as plt
-
 import sys 
 import os
 
 # plot settings (copied& from another folder)
-
 from config import plot_settings as ps
+
+#lapicques RC modle snntorch.Lapicqe 
+#1st order modle snntorch.Leaky 
+#synaptic Conducrtance-based neuron model snntorch.Synaptic
+
+
+#------------------------------------------------------------------------
+#________________________________________________________________________
+#Lapicques LIF neuron model
+#________________________________________________________________________
+#------------------------------------------------------------------------
+
+#trime constant of 5E-3 s
+
+time_steps=1e-3
+R=5
+C=1e-3
+
+#LIF neuron, au 5e-3
+lif1=snn.Lapique(R=R, C=C, time_step=time_step0)
 
