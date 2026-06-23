1ecf4d91e1de04f3278788d1575074e90f05b880
Amazing-Sage <baker.26.virginia@gmail.com>
2026-06-23 11:53:06 -0700

    adding tutorial5 and mnist dataset

---
+++ tutorial5.py
@@ -0,0 +1,117 @@
+#########################################################################
+# THIS IS TUTORIAL 4 FROM:                                              #
+# https://snntorch.readthedocs.io/en/latest/tutorials/tutorial_4.html   #
+# Focuses on Feedforward Spiking Neural Network                         #       
+#########################################################################
+
+#========================================================================
+#Importing packages and setting up enviroments 
+#========================================================================
+
+import snntorch as snn
+from snntorch import spikeplot as splt
+from snntorch import spikegen
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+import sys 
+import os
+
+# plot settings (copied& from another folder)
+from config import plot_settings as ps
+
+#========================================================================
+#Overcoming Dead Neuron Problem
+#========================================================================
+print("\nOvercoming Dead Neuron Problem")
+print("--------------------------------\n")
+
+# Leaky neuron model, overriding the backward pass with a custom function
+class LeakySurrogate(nn.Module):
+  def __init__(self, beta, threshold=1.0):
+      super(LeakySurrogate, self).__init__()
+
+      # initialize decay rate beta and threshold
+      self.beta = beta
+      self.threshold = threshold
+      self.spike_gradient = self.ATan.apply
+
+  # the forward function is called each time we call Leaky
+  #updates membrane pot to see if it should spike
+  def forward(self, input_, mem):
+    spk = self.spike_gradient((mem-self.threshold))  
+        # call the Heaviside function
+        #checks if V crossed threshold if it did it output1
+    reset = (self.beta * spk * self.threshold).detach()  # remove reset from computational graph
+    mem = self.beta * mem + input_ - reset  
+        # Eq (1), new input added and subtracts E if it just spiked (reset)
+    return spk, mem
+
+  # Forward pass: Heaviside function
+  # Backward pass: Override Dirac Delta with the derivative of the ArcTan function
+  @staticmethod
+  class ATan(torch.autograd.Function):
+      @staticmethod
+      def forward(ctx, mem):
+          spk = (mem > 0).float() 
+            # Heaviside on the forward pass: Eq(2)
+            #sharp step function, souly 1 or 0 no half spikes 
+          ctx.save_for_backward(mem)  
+            # store the membrane for use in the backward pass
+            #instead of sharp function, use equation (in backward def below)
+            #to create a smooth curve over spike point
+          return spk
+
+      @staticmethod
+      def backward(ctx, grad_output):
+          (spk,) = ctx.saved_tensors  # retrieve the membrane potential
+          grad = 1 / (1 + (np.pi * mem).pow_(2)) * grad_output # Eqn 5
+          return grad_output
+      
+# when neuron is built there's wo ways we can do this 
+
+lif1 = LeakySurrogate(beta=0.9) #can be simulated using a forloop 
+
+lif1 = snn.Leaky(beta=0.9) #the ATan surrogate gradient is applied to it by default 
+
+#========================================================================
+#Setting Up The Static MNIST DataSet
+#========================================================================
+print("\nSetting Up The Static MNIST DataSet")
+print("-------------------------------------\n")
+
+#same thing as the quick set up, we need this data set 
+#so we can train it by importing images for it to use
+
+# dataloader arguments
+batch_size = 128
+data_path='/tmp/data/mnist'
+
+dtype = torch.float
+
+ if torch.cuda.is_available(): 
+    device= torch.device("cuda")
+else:
+    device=torch.device("cpu") # write cpu bc it's on windows not mac
+    
+# Define a transform
+transform = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0,), (1,))])
+
+mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
+mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
+
+# Create DataLoaders
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=True)
